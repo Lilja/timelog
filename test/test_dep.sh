8102efe5cdc1d254e56c4c529ab50962c5a241a3
Erik Lilja <erik.lilja@qvantel.com>
2017-07-21 12:44:27 +0200

    Add dependecy check
    This closes #47

--- test/test_dep.sh
+++ test/test_dep.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -L "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/shunit2/shunit2-2.1.6.tgz" | tar zx
+if [ ! -f "shunit2-2.1.6.tgz" ]; then
+  curl -L "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/shunit2/shunit2-2.1.6.tgz" | tar zx
+fi
