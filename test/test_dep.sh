88a291f3bb332fafcca3d978d4c46aac82c45e94
Erik Lilja <6134511+Lilja@users.noreply.github.com>
2017-08-10 09:00:38 +0200

    Additional fixes

--- test/test_dep.sh
+++ test/test_dep.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-if [ ! -f "shunit2-2.1.6.tgz" ]; then
-  curl -L "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/shunit2/shunit2-2.1.6.tgz" | tar zx
+dir_path=${1:-"$PWD"}
+tgz_path=${2:-/tmp/shunit2-2.1.6.tgz}
+
+file_name="shunit2-2.1.6"
+
+if [ ! -d "$dir_path/$file_name" ]; then
+  curl -L "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/shunit2/shunit2-2.1.6.tgz" -o "$tgz_path"
+  tar zxf "$tgz_path" -C "$dir_path"
 fi
