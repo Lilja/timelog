4ee713edbf8fe253325934a28ea1992e2d226488
Erik Lilja <erikvlilja@gmail.com>
2017-08-29 00:13:12 +0200

    Trash

---
+++ test/mac_kcov.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+brew install zlib bash cmake pkgconfig
+git clone https://github.com/simonkagstrom/kcov /tmp
+#cd /tmp/kcov && mkdir build && cmake -G Xcode ../
+#xcodebuild -configuration Release
+
+#/tmp/kcov/src/Release/kcov --version
